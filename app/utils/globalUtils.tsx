41302cfbd3cda10f6e4085c7a9ecd8d7116c912f
Tram Nguyen <designertvn@gmail.com>
2025-03-05 11:14:34 -0600

    Copy edits and additional versioning for Summary

--- app/utils/globalUtils.tsx
+++ app/utils/globalUtils.tsx
@@ -1,19 +1,21 @@
 export const BASE_URL = "https://frontend-take-home-service.fetch.com";
 
 export function formatList(list: string[]): string {
+  const pluralList = list.map(dog => dog + 's');
+  
   if (list.length === 1) {
-    return list[0];
+    return pluralList[0];
   } else if (list.length === 2) {
-    return `${list[0]} and ${list[1]}}`;
+    return `${pluralList[0]} and ${pluralList[1]}`;
   } else {
     // List formatted with Oxford comma
-    return `${list.slice(0, -1).join(', ')}, and ${list[list.length - 1]}`;
+    return `${pluralList.slice(0, -1).join(', ')}, and ${pluralList[list.length - 1]}`;
   }
 }
 
 export function getAgeDisplay(age: number): string {
   if (age < 1) {
-    return "Under a year old"
+    return "Less than a year old"
   } else if (age === 1) {
     return "1 year old"
   } else {
--- app/utils/summaryUtils.tsx
+++ app/utils/summaryUtils.tsx
@@ -4,12 +4,22 @@ import styles from '@/app/components/dogs/summary.module.css';
 
 function getAgeRangeText(ageMin: number,
   ageMax: number ): string {
-  if (ageMin === ageMax) {
-    return `that are ${ageMin} years old`
+  if (ageMin === 0 && ageMax == 0) {
+    return `that are less than a year old`;
+  } else if (ageMin === 1 && ageMax == 1) {
+    return `that are a year old`;
+  } else if (ageMin === ageMax) {
+    return `that are ${ageMin} years old`;
+  } else if (ageMin === 0 && ageMax === 31) {
+    return '';
   } else if (ageMin === 0) {
-    return `that are up to ${ageMax} years old`
+    return `up to ${ageMax} years old`;
+  } else if (ageMin === 1 && ageMax === 31) {
+    return `that are at least a year old`;
+  } else if (ageMax === 31) {
+    return `that are at least ${ageMin} years old`;
   } else {
-    return `that are between ${ageMin} and ${ageMax} years old`
+    return `between ${ageMin} and ${ageMax} years old`
   }
 }
 
@@ -17,16 +27,18 @@ export function querySummary(query: Query, resultsCount: number) {
   const { breeds, sort, ageMin, ageMax } = query;
   const resultsDisplay = resultsCount.toLocaleString(); 
   const searchAllBreeds = breeds.length === 0 || breeds[0] === 'any';
-  const breedListDisplay = searchAllBreeds ? 'all breeds' : formatList(breeds);
+  const breedListDisplay = searchAllBreeds ? 'all dogs' : formatList(breeds);
   const breedSort = sort.breed === 'asc' ? 'ascending' : 'descending';
   const ageRangeDisplay = getAgeRangeText(ageMin, ageMax);
   const validAgeRange = ageMax >= ageMin;
 
-  return <>
-    { validAgeRange ?
-      <p>Showing <span className={styles.underline}>{resultsDisplay}</span> matches for: {breedListDisplay} {ageRangeDisplay} (in {breedSort} order by breed)</p> :
-      <p>That&apos;s not a valid age range. Please update your age filters.</p>}
-  </>
+  if (!validAgeRange) {
+    return <p>That&apos;s not a valid age range. Please update your age filters.</p>;
+  } else if (resultsCount === 0) {
+    return <p>No dogs matched your criteria: {breedListDisplay} {ageRangeDisplay}</p>
+  } else {
+    return <p>Showing <span className={styles.underline}>{resultsDisplay}</span> matches for: {breedListDisplay} {ageRangeDisplay} (in {breedSort} order by breed)</p>
+  }
 }
 
 export function resultsRangeDisplay(currentPage: number, resultsCount: number): string {
